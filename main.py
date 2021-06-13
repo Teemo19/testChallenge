0e102f97a2eca1e5f478bac79a547c44a44bde1b
teemo19 <cmaldonado0@ucol.mx>
2021-06-13 12:07:06 -0500

    authors route

--- main.py
+++ main.py
@@ -115,3 +115,21 @@ def create_author(current_user):
 
     return jsonify({'message': 'new author created'})
 
+@app.route('/authors', methods=['POST', 'GET'])
+@token_required
+def get_authors(current_user):
+
+    authors = Authors.query.filter_by(user_id=current_user.id).all()
+
+    output = []
+    for author in authors:
+
+           author_data = {}
+           author_data['name'] = author.name
+           author_data['book'] = author.book
+           author_data['country'] = author.country
+           author_data['booker_prize'] = author.booker_prize
+           output.append(author_data)
+
+     return jsonify({'list_of_authors' : output})
+
