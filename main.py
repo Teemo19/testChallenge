f2de7bfce1d629a5cfeeb1811b58b7fc3fc37122
teemo19 <cmaldonado0@ucol.mx>
2021-06-13 14:03:13 -0500

    user authentication finished

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uuid
 import jwt
 import datetime
 import os
+import requests
 from functools import wraps
 
 app = Flask(__name__)
@@ -57,14 +58,11 @@ def token_required(f):
 @app.route('/register', methods=['GET', 'POST'])
 def signup_user():
     data = request.get_json()
-
     hashed_password = generate_password_hash(data['password'], method='sha256')
-
     new_user = Users(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
-
-    return jsonify({'message': 'registered successfully'})
+    return jsonify(data)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -75,13 +73,11 @@ def login_user():
         return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
 
     user = Users.query.filter_by(name=auth.username).first()
-
     if check_password_hash(user.password, auth.password):
         token = jwt.encode(
             {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
             app.config['SECRET_KEY'])
-        return jsonify({'token': token.decode('UTF-8')})
-
+        return jsonify({'token': token.encode().decode('UTF-8')})
     return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
 
 
@@ -102,50 +98,24 @@ def get_all_users():
 
     return jsonify({'users': result})
 
-
-@app.route('/author', methods=['POST', 'GET'])
+@app.route('/exchange_rate', methods=['GET'])
 @token_required
-def create_author(current_user):
-    data = request.get_json()
+def exchange_rate():
 
-    new_authors = Authors(name=data['name'], country=data['country'], book=data['book'], booker_prize=True,
-                          user_id=current_user.id)
-    db.session.add(new_authors)
-    db.session.commit()
-
-    return jsonify({'message': 'new author created'})
-
-@app.route('/authors', methods=['POST', 'GET'])
-@token_required
-def get_authors(current_user):
-
-    authors = Authors.query.filter_by(user_id=current_user.id).all()
-
-    output = []
-    for author in authors:
-
-           author_data = {}
-           author_data['name'] = author.name
-           author_data['book'] = author.book
-           author_data['country'] = author.country
-           author_data['booker_prize'] = author.booker_prize
-           output.append(author_data)
-
-     return jsonify({'list_of_authors' : output})
-
-@app.route('/authors/<author_id>', methods=['DELETE'])
-@token_required
-def delete_author(current_user, author_id):
-    author = Author.query.filter_by(id=author_id, user_id=current_user.id).first()
-    if not author:
-       return jsonify({'message': 'author does not exist'})
-
-
-    db.session.delete(author)
-    db.session.commit()
-
-    return jsonify({'message': 'Author deleted'})
-
+    return {
+            "rates":
+                {
+                    "provider_1": {
+                    "last_updated": "2018-04-22T18:25:43.511Z",
+                    "value": 20.4722
+                    },
+                    "provider_2_variant_1":
+                        {
+                            "last_updated": "2018-04-23T18:25:43.511Z",
+                            "value": 20.5281
+                        }
+                }
+            }
 
 if  __name__ == '__main__':
      app.run(debug=True)
