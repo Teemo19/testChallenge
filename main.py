0b870fd2aabb2008dba48a421033aa7394d7f64a
teemo19 <cmaldonado0@ucol.mx>
2021-06-13 12:05:42 -0500

    users route

--- main.py
+++ main.py
@@ -83,3 +83,21 @@ def login_user():
         return jsonify({'token': token.decode('UTF-8')})
 
     return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = Users.query.all()
+
+    result = []
+
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+
+        result.append(user_data)
+
+    return jsonify({'users': result})
